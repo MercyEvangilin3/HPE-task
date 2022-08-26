ce3703b75b43e71f96ffd3e1f00a94110d6ff5c6
Krishnapuram Bhumireddy <mercy-evangilin.krishnapuram-bhumireddy@capgemini.com>
2022-08-26 16:45:36 +0530

    task1

---
+++ log4net/log4net/AbstractClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net
+{
+    public abstract class Shape
+    {
+        public abstract int Area();
+
+
+    }
+    public class Rectangle : Shape
+    {
+          public override int Area()
+        {
+            Console.WriteLine("Enter Length of Rectangle:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Breadth of Rectangle:");
+            int breadth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+            int Area = length * breadth;
+
+            return Area;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+
+        public override int Area()
+        {
+            Console.WriteLine("Enter Base of Triangle:");
+            int Base = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Height of Triangle:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            int area = (Base * height) / 2;
+            return area;
+        }
+
+
+
+    }
+
+}
---
+++ log4net/log4net/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace log4net
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            try
+            {
+                Rectangle r = new Rectangle();
+                int rectangle = r.Area();
+                Triangle tr = new Triangle();
+                int triangle = tr.Area();
+               log.Info($"Area of Rectangle is {rectangle}");
+                Console.WriteLine("\n");
+               log.Info($"Area of Triangle is {triangle}");
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+     }
+}
+
+;
