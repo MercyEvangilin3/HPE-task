37759f7e43ccfbd512372d2641533112979c7bd5
Krishnapuram Bhumireddy <mercy-evangilin.krishnapuram-bhumireddy@capgemini.com>
2022-09-20 17:29:43 +0530

    MultiThreading Task1

---
+++ MultiThreading/MultiThreading/Caluclator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using log4net;
+namespace MultiThreading
+{
+    public class Caluclator
+    {
+
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        int num1, num2;
+        public Caluclator(int a,int b)
+        {
+            num1 = a;
+            num2 = b;
+            
+        }
+       public void Addition()
+        {
+            Thread.Sleep(2000);
+            int result = num1 + num2;
+            Console.WriteLine("Addition of   a and b is:"+ result);
+            
+        }
+
+        public void Subraction()
+        {
+            Thread.Sleep(3000);
+            int result = num1 - num2;
+            Console.WriteLine("Subraction of a and b is:"+result);
+            //Console.ReadLine();
+
+        }
+
+        public void Multiplication()
+        {
+            Thread.Sleep(4000);
+            int result = num1*num2;
+            Console.WriteLine("Multiplication of a and b is:" + result);
+        }
+
+        public void Division()
+        {
+            Thread.Sleep(5000);
+            int result = num1 / num2;
+            Console.WriteLine("divison of a and b is:" + result);
+        }
+    }
+}
---
+++ MultiThreading/MultiThreading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+namespace MultiThreading
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the First value:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second value:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+
+            Caluclator cs = new Caluclator(a,b);
+            Thread t1 = new Thread(cs.Addition);
+            Thread t2 = new Thread(cs.Subraction);
+            Thread t3 = new Thread(cs.Multiplication);
+            Thread t4 = new Thread(cs.Division);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            Console.ReadLine();
+
+        }
+    }
+}
