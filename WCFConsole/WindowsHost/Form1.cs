22ab3a32477e2c61a38ed085ff71873b973a2f52
Krishnapuram Bhumireddy <mercy-evangilin.krishnapuram-bhumireddy@capgemini.com>
2022-09-28 12:25:45 +0530

    windows Service

---
+++ WCFConsole/WindowsHost/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+namespace WindowsHost
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost host;
+        public Form1()
+        {
+            InitializeComponent();
+            host = new ServiceHost(typeof(WCFConsole.WCFService));
+            host.Open();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            lblMessage.Text = "Service Started";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            host = new ServiceHost(typeof(WCFConsole.WCFService));
+            host.Open();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            lblMessage.Text = "Service Started";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            host.Close();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            lblMessage.Text = "Service Stopped";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            host.Close();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+    }
+}
---
+++ WCFConsole/WindowsServiceHost/HelloWindowService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WindowsServiceHost
+{
+    public partial class HelloWindowService : ServiceBase
+    {
+        ServiceHost host;
+        public HelloWindowService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(WCFConsole.WCFService));
+            host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            host.Close();
+        }
+    }
+}
